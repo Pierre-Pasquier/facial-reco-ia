a6e61bd1fc033a33a93b7015f9b00f502590e671
Becquet Albert <albert.becquet@telecomnancy.eu>
2023-12-07 22:01:24 +0100

    evaluation script

--- src/library_test/face_recognition_lib/face_detector_test.py
+++ src/library_test/face_recognition_lib/face_detector_test.py
@@ -1,21 +1,36 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+
 import time
 import click
-import os
 import glob
 import face_recognition
+import json
 from statistics import mean
+
+from src.evaluation.evaluation import evaluation
+
 
 @click.command()
 @click.option('--folder_path', type=str, help='Path to the image folder', required=True)
-def main(folder_path):
+@click.option('--label_path', type=str, help='Path to the label json file', required=False)
+@click.option('--iou_threshold', type=float, help='IoU threshold for evaluation', required=False, default=0.5)
+def main(folder_path, label_path, iou_threshold):
     processing_times = []
     
     for f in glob.glob(os.path.join(folder_path, "*.jpg")):
         image = face_recognition.load_image_file(f)
+
         start = time.time()
-        face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model="cnn")
+
+        face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0) #, model="cnn")
+
         processing_times.append(time.time() - start)
-        print(f"\nFile {f}:")
+
+        file_name = f.split("/")[-1]
+
+        print(f"\nFile {file_name}:")
         print("Number of faces detected: {}".format(len(face_locations)))
         print(f"Processing time: {processing_times[-1]}")
 
@@ -23,6 +38,13 @@ def main(folder_path):
 
             top, right, bottom, left = face_location
             print(f"Detection {k}: Left: {left} Top: {top} Right: {right} Bottom: {bottom}")
+
+        if label_path is not None:
+
+            with open(label_path, 'r') as file:
+                label_dict = json.load(file)
+
+            evaluation(label_dict[file_name], face_locations, iou_threshold)
 
     print("Average time taken:", mean(processing_times[1:]))
 
