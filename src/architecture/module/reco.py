d9d13c46fedc1d1e629ff0262c08bae9c6fe605f
Becquet Albert <albert.becquet@telecomnancy.eu>
2023-12-15 12:20:31 +0100

    face reco block test

--- src/architecture/module/reco.py
+++ src/architecture/module/reco.py
@@ -1,17 +1,24 @@
 import os
-import cv2
 import dlib
 import glob
 
 
 shape_predictor = 'models/shape_predictor_5_face_landmarks.dat'
 face_reco = 'models/dlib_face_recognition_resnet_model_v1.dat'
-folder_path = 'src/architecture/images/temp'
+folder_path = 'src/architecture/images'
 
 sp = dlib.shape_predictor(shape_predictor)
 facerec = dlib.face_recognition_model_v1(face_reco)
 
-for f in glob.glob(os.path.join(folder_path, "*.jpg")):
+person_vectors = []
+
+for f in glob.glob(os.path.join(folder_path, 'persons', "*", "*.txt")):
+    with open(f, 'r') as file:
+        person_vectors += [
+            eval(file.readline())
+        ]
+
+for f in glob.glob(os.path.join(folder_path, 'temp', "*.jpg")):
     img = dlib.load_rgb_image(f)
 
     height, width = img.shape[0], img.shape[1]
@@ -22,3 +29,5 @@ for f in glob.glob(os.path.join(folder_path, "*.jpg")):
 
     face_chip = dlib.get_face_chip(img, shape)
     face_descriptor = facerec.compute_face_descriptor(face_chip)
+
+    print(face_descriptor)
