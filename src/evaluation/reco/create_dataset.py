b07f84a85404b78905afdd2b5c8c2f3c3ae67b1c
Becquet Albert <albert.becquet@telecomnancy.eu>
2024-01-24 14:07:31 +0100

    reco performances evaluation

--- src/evaluation/reco/create_dataset.py
+++ src/evaluation/reco/create_dataset.py
@@ -2,6 +2,7 @@ import dlib
 import cv2
 import glob
 import os
+import shutil
 import random
 
 
@@ -11,20 +12,20 @@ def save_cropped_faces(faces, image, file, exit_folder):
         print(face)
         (x, y, w, h) = (face.left(), face.top(), face.width(), face.height())
         cropped_face = image[y:y+h, x:x+w]
-        os.makedirs(exit_folder, exist_ok=True)
-        cv2.imwrite(os.path.join(exit_folder, os.path.basename(file)), cropped_face)
+        if x >= 0 and y >= 0 and w >= 0 and h >= 0:
+            os.makedirs(exit_folder, exist_ok=True)
+            cv2.imwrite(os.path.join(exit_folder, os.path.basename(file)), cropped_face)
 
 def crop_directory(entry_folder, exit_folder):
     detector = dlib.get_frontal_face_detector()
-
-    # TODO
-    print(entry_folder, exit_folder)
 
     ### IMAGE PROCESSING LOOP ###
     for f in glob.glob(os.path.join(entry_folder, "*.jpg")):
         img = dlib.load_rgb_image(f)
 
         dets = detector(img, 1)
+
+        print(dets)
 
         if dets != []:
             save_cropped_faces(dets, img, f, exit_folder)
@@ -35,15 +36,24 @@ def crop_dataset(dataset_folder, new_dataset_path, nb_folder):
     for _ in range(nb_folder):
         random_directory = random.choice(all_directories)
 
-        # TODO
-        print(random_directory)
-
         crop_directory(os.path.join(dataset_folder, random_directory), os.path.join(new_dataset_path, random_directory + "_cropped"))
 
         all_directories.remove(random_directory)
 
+def copy_all_images(dataset_path):
+    os.makedirs(os.path.join(dataset_path, "_all"), exist_ok=True)
+
+    all_directories = [d for d in os.listdir(dataset_path) if os.path.isdir(os.path.join(dataset_path, d))]
+
+    for dir in all_directories:
+        if dir != "_all":
+            for f in os.listdir(os.path.join(dataset_path, dir)):
+                shutil.copy(os.path.join(dataset_path, dir, f), os.path.join(dataset_path, "_all", f))
+
+
 def main(dataset_folder, new_dataset_path, nb_folder):
     crop_dataset(dataset_folder, new_dataset_path, nb_folder)
+    copy_all_images(new_dataset_path)
 
 
 if __name__ == "__main__":
